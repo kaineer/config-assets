e2e44cc8663e4e98e260d41b5a213f8c279d3b59
Sergey Kluchkovsky <kaineer@gmail.com>
2024-01-08 15:55:47 +0500

    Add yaml test

--- bin/python/test/test_yaml.py
+++ bin/python/test/test_yaml.py
@@ -1,3 +1,8 @@
-from yaml import load, dump
+from yaml import load, SafeLoader
 
-
+def test_load():
+    source = "name: John\nage: 30\nimportant: true\n"
+    result = load(source, Loader=SafeLoader)
+    assert result["name"] == "John"
+    assert result["age"] == 30
+    assert result["important"]
